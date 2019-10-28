bb9956d47bb6b8c2ea9ef627650c914765a699fd
mbrandaobsolus <mbrandao@bsolus.pt>
2019-10-28 17:04:35 +0000

    20191028 170426

--- vars/FullTestStages.groovy
+++ vars/FullTestStages.groovy
@@ -9,16 +9,7 @@ def call(Map config) {
                     }
                 }
                 steps {
-                    sh """
-                        /root/re-install/run.sh \
-                        --proj-name ${config.BEEVO_PROJECT_NAME} \
-                        --proj-env ${config.BEEVO_PROJECT_ENV} \
-                        --envi automatedtest \
-                        --mysql-db-prefix ${config.BEEVO_PROJECT_BD_PREFIX} \
-                        --mysql-dump-ip ${config.BEEVO_PRODUCTION_IP} \
-                        --mysql-dump-name ${config.BEEVO_PRODUCTION_BDNAME} \
-                        --prod-url ${config.BEEVO_PRODUCTION_URL} 
-                    """
+                    Reinstall( config )
                 }
             }
             /*stage('Install project'){
