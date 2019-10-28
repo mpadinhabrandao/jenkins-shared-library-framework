d50a6d3636eab97a33a0cf03664f7cacd2b399f6
mbrandaobsolus <mbrandao@bsolus.pt>
2019-10-28 16:50:39 +0000

    20191028 165026

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -9,16 +9,16 @@ def call(Map config) {
                     }
                 }
                 steps {
-                    sh '''
+                    sh "
                         /root/re-install/run.sh \
-                        --proj-name ${config.PROJECT_MACHINE_LABEL} \
+                        --proj-name ${config.BEEVO_PROJECT_NAME} \
                         --proj-env ${config.BEEVO_PROJECT_ENV} \
                         --envi automatedtest \
                         --mysql-db-prefix ${config.BEEVO_PROJECT_BD_PREFIX} \
                         --mysql-dump-ip ${config.BEEVO_PRODUCTION_IP} \
                         --mysql-dump-name ${config.BEEVO_PRODUCTION_BDNAME} \
                         --prod-url ${config.BEEVO_PRODUCTION_URL} 
-                    '''
+                    "
                 }
             }
             /*stage('Install project'){
